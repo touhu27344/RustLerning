5aa106024a8564a35d723408078259ca2ee25ba0
touhu27344 <tk27344@icloud.com>
2023-11-14 01:09:29 +0900

    変数、標準入力、標準出力、preludeを学びました。

---
+++ gurssing_game/target/debug/deps/gurssing_game.d
@@ -0,0 +1,5 @@
+G:\マイドライブ\RustLerning\gurssing_game\target\debug\deps\gurssing_game.exe: src\main.rs
+
+G:\マイドライブ\RustLerning\gurssing_game\target\debug\deps\gurssing_game.d: src\main.rs
+
+src\main.rs:
---
+++ gurssing_game/target/debug/gurssing_game.d
@@ -0,0 +1,1 @@
+G:\マイドライブ\RustLerning\gurssing_game\target\debug\gurssing_game.exe: G:\マイドライブ\RustLerning\gurssing_game\src\main.rs
