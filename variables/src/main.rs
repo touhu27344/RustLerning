7c3979af7cd7bbfa425630d06e5b40d7fef11dbf
touhu27344 <tk27344@icloud.com>
2023-11-15 15:00:00 +0900

    if文を学びました。

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -46,7 +46,7 @@ fn main() {
     //
     //関数
     //
-
+    /*
     //関数の定義 (関数_i)
     println!("Hello, world!");
 
@@ -58,7 +58,34 @@ fn main() {
     //戻り値のある関数(関数_iii)
     let x = plus_one(5);
     println!("The value of x is:{}", x);
+    */
 
+    //
+    //if文
+    //
+
+    //ifとelse if
+    let number = 7;
+
+    if number % 4 == 0 {
+        // 数値は4で割り切れます
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        // 数値は3で割り切れます
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        // 数値は2で割り切れます
+        println!("number is divisible by 2");
+    } else {
+        // 数値は4、3、2で割り切れません
+        println!("number is not divisible by 4, 3, or 2");
+    }
+
+    //let文内でif式を扱う
+    let condition = true;
+    let number = if condition {5} else {6};
+
+    println!("The value of number is:{}",number);
 }
 
 //関数_i
