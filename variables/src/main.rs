a9027c3111b4d66ed599cdb0aa5c5380e764c3d3
touhu27344 <tk27344@icloud.com>
2023-11-15 15:09:44 +0900

    loop文を学びました。

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -63,7 +63,7 @@ fn main() {
     //
     //if文
     //
-
+    /*
     //ifとelse if
     let number = 7;
 
@@ -86,6 +86,57 @@ fn main() {
     let number = if condition {5} else {6};
 
     println!("The value of number is:{}",number);
+    */
+
+    //
+    //loop 文
+    //
+
+    //loopラベルを用いたbreak,continue
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {}", count);
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {}", remaining);
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {}", count);
+
+    //while文
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{}!", number);
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+
+    //要素を用いたfor文　
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {}", element);
+    }
+
+    //while文のコードの改良
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
+
 }
 
 //関数_i
