d6609dbeab617eaa97b5043a7bf1369ea9fc7e6c
touhu27344 <tk27344@icloud.com>
2023-11-16 13:51:04 +0900

    関数とスコープについて学びました。

--- Chapter4/src/main.rs
+++ Chapter4/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     //
     //String
     //
-
+    /*
     //String型の生成
     let s = String::from("Hello");
     println!("{}",s);
@@ -23,7 +23,44 @@ fn main() {
 
     //println!("{}",s1); //パニック
     println!("s2 = {}, s3 = {}",s2,s3); //cloneしたものだと良い。
+    */
 
+    //
+    //関数とスコープ
+    //
 
+    let s = String::from("hello"); //sスコープに入る
+    takes_ownership(s);            //sが関数にムーブ
+　//println!("{}",s);          //ERR,sがスコープを抜けている。
 
+    let x = 5;
+    makes_copy(x);
+
+    //
+    //戻り値とスコープ
+    //
+
+    let s1 = gives_ownership();   //gives_ownership => s1　でムーブ
+
+    let s2 = String::from("hello");  // s2 がスコープに入る。
+
+    let s3 = takes_and_gives_backs(s2); //s2 => takes_and_gives_backs　でムーブ、さらに　takes_and_gives_backs => s3　でムーブ
+
+}
+
+fn takes_ownership(some_string: String) {
+    println!("{}", some_string);
+}
+
+fn makes_copy(some_integer: i32) {
+    println!("{}",some_integer);
+}
+
+fn gives_ownership() -> String {
+    let some_string = String::from("hello");
+    some_string
+}
+
+fn takes_and_gives_backs(a_string: String) -> String {
+    a_string
 }
